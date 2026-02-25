42e91a1fb850a5031d8fe52896fed656c2509877
praveenknps1 <praveenchinnapraveenchinna246@gmail.com>
2026-02-25 10:53:23 +0530

    price change

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,28 +2,28 @@ const products = [
   {
     id: 1,
     name: "iPhone",
-    price: 70000,
+    price: 69999,
     image:
       "https://th.bing.com/th?q=iPhone+13+Launch+Date+in+India+Price&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.3&pid=InlineBlock&rm=3&defcache=1&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247",
   },
   {
     id: 2,
     name: "Samsung",
-    price: 65000,
+    price: 65999,
     image:
       "https://th.bing.com/th/id/OIP.7mEURRbxsBPUvlq_-K4LOAHaHa?w=174&h=180&c=7&r=0&o=7&cb=defcache2&dpr=1.3&pid=1.7&rm=3&defcache=1",
   },
   {
     id: 3,
     name: "OnePlus",
-    price: 60000,
+    price: 69999,
     image:
       "https://th.bing.com/th/id/OIP.4tmbMWtRXKLs1ZHTYL36dQHaHa?w=172&h=180&c=7&r=0&o=7&cb=defcache2&dpr=1.3&pid=1.7&rm=3&defcache=1",
   },
   {
     id: 4,
     name: "Google Pixel",
-    price: 55000,
+    price: 55999,
     image:
       "https://tse2.mm.bing.net/th/id/OIP.pUcq7r7vKYosbXMnXocESgHaHa?cb=defcache2&pid=ImgDet&defcache=1&w=187&h=187&c=7&dpr=1.3&o=7&rm=3",
   },
